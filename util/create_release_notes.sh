b40e716d08dafac0adc416e1df92b53e64eff584
Vamshi Basupalli <vamshi_kr@hotmail.com>
2016-10-12 14:56:01 -0500

    Updated release/RELEASE_NOTES.txt and util/create_release_notes.sh

--- util/create_release_notes.sh
+++ util/create_release_notes.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 function main {
-	
-	local GIT_TAG=$1
-	local FILENAME="$PWD/release/RELEASE_NOTES.txt"
-	
+	local  GIT_TAG="${1:-$(git tag | tail -n 1)}"
+	local FILENAME="release/RELEASE_NOTES.txt"
+
 	if test -f "${FILENAME}"; then
 		if test "$(uname -s)" == "Darwin"; then
 			MTIME="$(stat -f '%m' ${FILENAME})"
@@ -12,33 +11,33 @@ function main {
 			MTIME="$(stat --format='%Y' ${FILENAME} )"
 		fi
 		
-		if test $# -gt 1; then
-			NEW_FILENAME="temp_release_notes.txt"
-			#NEW_FILENAME="/dev/stdout"
-		else
-			NEW_FILENAME="temp_release_notes.txt"
-		fi
+		# if test $# -gt 0; then
+		# 	NEW_FILENAME="temp_release_notes.txt"
+		# else
+		# 	NEW_FILENAME="/dev/stdout"
+		# fi
+		#
+		local NEW_FILENAME="temp_release_notes.txt"
 
 		if test "$(git log --since=${MTIME} --pretty=fuller --format='%B')" != ""; then
 			SEP=$(python3 -c "print('-'*35)")
 			cat > "${NEW_FILENAME}" <<EOF
 ${SEP}
-ruby-assess version ${GIT_TAG} ($(date))
+$(basename $PWD) version ${GIT_TAG} ($(date))
 ${SEP}
-$(git log --since=${MTIME} --pretty=fuller --format='%B' | sed -E 's:.+:- &:')
+$(git log --since=${MTIME} --pretty=fuller --format='%B' | sed -r '/^[^-] .+/d' | sed -r 's:.+:- &:')
 
 $(cat ${FILENAME})
 EOF
 
-		if test "${NEW_FILENAME}" != "/dev/stdout" -a -f "${NEW_FILENAME}"; then
-			mv "${NEW_FILENAME}" "${FILENAME}"
-    	fi
+			if test "${NEW_FILENAME}" != "/dev/stdout" -a -f "${NEW_FILENAME}"; then
+				mv "${NEW_FILENAME}" "${FILENAME}"
+			fi
 		fi
 	fi
 
 }
 
+#set -x
 
-: ${1:?"Usage: $0 <release-tag>"}
-
-main $@
+main "$@"
